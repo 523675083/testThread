450feb56f89171c828b83a6cb96b13436a197e87
zhoupei <zhoupei@yktour.com.cn>
2018-09-12 22:44:01 +0800

    111

--- src/com/test.java
+++ src/com/test.java
@@ -9,6 +9,7 @@ public class test {
     public static void main(String[] args){
         List<String> list = new ArrayList<String>();
         list.add("1");
+
         list.add("2");
         list.add("3");
 //        for (String item : list) {
